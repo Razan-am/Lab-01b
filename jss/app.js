4e1fecdbfb2405f0db44d899805bdc8b64afa07d
razan-am <razanalamleh@gmail.com>
2021-04-25 23:22:51 +0300

    updated

---
+++ jss/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+alert('Hello')
+
+let q1=prompt('Please enter your full name');
+let ans1=q1
+console.log (ans1);
+
+
+let q2=prompt('Which continent you like to explore ?');
+let ans2=q2
+console.log (ans2);
+
+let q3=parseInt(prompt('How many people do you want to reserve for?'));
+let ans3=q3
+console.log (ans3);
+
+let q4=prompt('Would you like to start the tour ?(Y,N)');
+let ans4=q4
+console.log (ans4);
+
+alert('welcome'+" "+ans1+" "+'You have choose'+" "+ans2+" "+'The number of people is'+" "+ans3+" "+'lets start the tour')
+
